f450ae10edd47e143cd02ff3597b0ac5f235907d
paulwooolf <paulwooolf@gmail.com>
2021-10-13 23:55:10 +0300

    display categories

--- src/app/views/categories/categories.component.ts
+++ src/app/views/categories/categories.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {DataHandlerService} from "../../service/data-handler.service";
+import {Category} from "../../model/Category";
 
 @Component({
-  selector: 'app-categories',
-  templateUrl: './categories.component.html',
-  styleUrls: ['./categories.component.scss']
+    selector: 'app-categories',
+    templateUrl: './categories.component.html',
+    styleUrls: ['./categories.component.scss']
 })
 export class CategoriesComponent implements OnInit {
 
-  constructor() { }
+    public categories?: Category[];
 
-  ngOnInit(): void {
-  }
+    constructor(private dataHandler: DataHandlerService) {
+
+    }
+
+    ngOnInit(): void {
+        this.categories = this.dataHandler.getCategories();
+        console.log(this.categories);
+    }
 
 }
